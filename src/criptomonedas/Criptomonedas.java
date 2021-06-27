a915ac1a843b1572453aa337fc2d5d5bccffec5f
ProfeJulio <73155080+ProfeJulio@users.noreply.github.com>
2021-06-26 23:50:03 -0400

    Update Criptomonedas.java
    segunda prueba de comando git-pull: se crea un comentario 2

--- src/criptomonedas/Criptomonedas.java
+++ src/criptomonedas/Criptomonedas.java
@@ -5,6 +5,7 @@ public class Criptomonedas {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         // esto es un comentario
+        // segundo comentario
     }
     
 }
