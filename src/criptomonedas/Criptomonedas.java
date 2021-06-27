b630c9ffc66419deae6a0d91a897d5c1188a2790
ProfeJulio <jmollom@cldvarica.cl>
2021-06-26 23:54:43 -0400

    Update Criptomonedas.java
    pueba de commit

--- src/criptomonedas/Criptomonedas.java
+++ src/criptomonedas/Criptomonedas.java
@@ -5,7 +5,7 @@ public class Criptomonedas {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         // esto es un comentario
-        // segundo comentario
+        // segundo comentario: prueba exitosa con comando git pull
     }
     
 }
