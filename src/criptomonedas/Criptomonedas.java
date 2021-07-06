04216fabd3ccb56105452bab3f2ed16107ae88e0
ProfeJulio <jmollom@cldvarica.cl>
2021-07-06 13:37:22 -0400

    Se crea la Clase Persona y dos objetos de la clase persona: Kevin y Petunia

--- src/criptomonedas/Criptomonedas.java
+++ src/criptomonedas/Criptomonedas.java
@@ -3,8 +3,11 @@ package criptomonedas;
 public class Criptomonedas {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-        System.out.println("Modificacion");
+        Persona Kevin = new Persona("Kevin", 17, "24.111.222-3", 'M', 100000);
+        Persona Petunia = new Persona("Petunia", 18, "23.444.333-5", 'F', 200000);
+        
+        System.out.println(Petunia.nombre);
+        System.out.println(Kevin.rut);
 
     }
     
---
+++ src/criptomonedas/Persona.java
@@ -0,0 +1,25 @@
+package criptomonedas;
+
+public class Persona {
+    //Atributos
+    String nombre;
+    int edad;
+    String rut;
+    char sexo; //M:Masculino,F:Femenino
+    float puduCoin;
+    
+    // Constructor
+    public Persona(String nom, int eda, String r, char sex, float pC){
+        nombre = nom;
+        edad = eda;
+        rut = r;
+        sexo = sex;
+        puduCoin = pC;
+    }
+    
+    // Metodo
+    public float abonarDinero(float monto){
+        puduCoin = puduCoin - monto;
+        return monto;  
+    }
+}
